682ea29765e6606e49b94a4704c05b7cbdef675a
Alejandrosch2 <alejandroschw99@gmail.com>
2022-02-07 20:10:50 -0300

    Created Navbar components

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+const NavBar = () => {
+
+    return(
+      <nav className="NavBar">
+          <div>
+                <p> MARKET-PLACE </p>
+                <img src="./images/logo192.png" alt="logo"/>
+          </div>
+        <button className="opcion">boton</button>
+      </nav>
+    )
+  
+  }
+
+  export default NavBar
