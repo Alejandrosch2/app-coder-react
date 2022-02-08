9d1740efe2abdbaf620b3426d9e8be78e6f863a5
Alejandrosch2 <alejandroschw99@gmail.com>
2022-02-07 21:15:02 -0300

    Nav changes

---
+++ src/components/NavBar/NavBars/NavBar.js
@@ -0,0 +1,24 @@
+import './NavBar.css'
+
+
+const NavBar = () => {
+
+    return(
+      <nav className="NavBar">
+          <div>
+                <p> Tienda-Online</p>
+              
+          </div>
+        <button className="option">accion</button>
+        <button className="option">estrategia</button>
+        <button className="option">FPS</button>
+        <button className="option">indie</button>
+        <button className="option">deportes</button>
+        <button className="option">cooperativo</button>
+
+      </nav>
+    )
+  
+  }
+
+  export default NavBar
